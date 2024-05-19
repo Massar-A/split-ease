c565c0d214c911eb229eceefae1d703693ced269
Massar <massarabbas62@gmail.com>
2024-05-19 23:00:40 +0200

    settings

--- SplitEase/SplitEase/settings.py
+++ SplitEase/SplitEase/settings.py
@@ -120,3 +120,7 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
